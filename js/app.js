6c41bd8d84571fabcd963e6436af1ddfb746699e
Alejandro Rivera <alexrivera78@gmail.com>
2022-05-15 22:24:37 -0500

    multiplication of values

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ function sum(a, b) {
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+// testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -28,6 +28,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) {
+  return [a * b, `The product of ${a} and ${b} is ${a * b}.`];
   //eslint-disable-line
 }
 
